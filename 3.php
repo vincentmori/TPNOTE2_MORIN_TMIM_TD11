54f33e46f5321f10c69a6f897ec528b438dc37e0
vincentmori <vincent.morin@edu.ece.fr>
2023-05-26 16:41:30 +0200

    3

--- 3.php
+++ 3.php
@@ -14,14 +14,66 @@ session_start();
 
 <body>
     <h1>Votre selection :</h1>
-    <form action="3Traitement.php" method="post">
+    <form action="3TraitementB.php" method="post">
         <table>
             <tr>
                 <td>
                     <input type="radio" name="choix" value="1">Supprimer l'employe 102<br>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="button1" value="Soumettre" />
+                </td>
+            </tr>
+        </table>
+    </form>
+    <form action="3TraitementC.php" method="post">
+        <table>
+            <tr>
+                <td>
                     <input type="radio" name="choix" value="2">Afficher les employes par ordre croissant de salaire<br>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="button1" value="Soumettre" />
+                </td>
+            </tr>
+        </table>
+    </form>
+    <form action="3TraitementE.php" method="post">
+        <table>
+            <tr>
+                <td>
                     <input type="radio" name="choix" value="3">Afficher le salaire maximum et minimum<br>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="button1" value="Soumettre" />
+                </td>
+            </tr>
+        </table>
+    </form>
+    <form action="3TraitementF.php" method="post">
+        <table>
+            <tr>
+                <td>
                     <input type="radio" name="choix" value="4">Afficher le salaire moyen et le nombre d'employe<br>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="button1" value="Soumettre" />
+                </td>
+            </tr>
+        </table>
+    </form>
+    <form action="3TraitementH.php" method="post">
+        <table>
+            <tr>
+                <td>
                     <input type="radio" name="choix" value="5">Afficher les employes dont le patron a un ID de 100<br>
                 </td>
             </tr>
