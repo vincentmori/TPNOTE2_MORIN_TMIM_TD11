b63a8f5ce522303a2bb4e403db823600d5d95865
Elliott Tmim <elliotttmim@gmail.com>
2023-05-26 16:55:05 +0200

    Isset -

--- 3TraitementE.php
+++ 3TraitementE.php
@@ -6,16 +6,6 @@ $database = "tpnote2";
 $db_handle = mysqli_connect("127.0.0.1:3306", "root", "");
 $db_found = mysqli_select_db($db_handle, $database);
 
-// Récupère données formulaire
-$ID = isset($_POST['ID']) ? $_POST["ID"] : "";
-$Prenom = isset($_POST['Prenom']) ? $_POST["Prenom"] : "";
-$Nom = isset($_POST['Nom']) ? $_POST["Nom"] : "";
-$DateEmbauche = isset($_POST['DateEmbauche']) ? $_POST["DateEmbauche"] : "";
-$ID_Travail = isset($_POST['ID_Travail']) ? $_POST["ID_Travail"] : "";
-$Salaire = isset($_POST['Salaire']) ? $_POST["Salaire"] : "";
-$ID_Patron = isset($_POST['ID_Patron']) ? $_POST["ID_Patron"] : "";
-$ID_Dept = isset($_POST['ID_Dept']) ? $_POST["ID_Dept"] : "";
-$Photo = isset($_POST['Photo']) ? $_POST["Photo"] : "";
 
 if ($db_found) {
     $sql = "SELECT MIN(Salaire) AS SalaireMin, MAX(Salaire) AS SalaireMax FROM employes"; // Requête pour salaire min et salaire max
